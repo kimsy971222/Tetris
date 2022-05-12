0105d5f9cb21a67967bfedd8653d757d56f4ec9f
kimsy971222 <kimsy@codecraft.co.kr>
2022-05-12 11:38:05 +0900

    블록 좌우 이동

--- js/tetris.js
+++ js/tetris.js
@@ -1,19 +1,56 @@
+const board = document.querySelector(".board");
+const rowCount = 20;
+const columnCount = 10;
+
 init();
 
-function init() {
-    const rowCount = 20;
-    const columnCount = 10;
+document.addEventListener("keydown", (e) => {
+    let type = e.key;
+    let movingElem = document.querySelectorAll(".moving");
+    switch (type) {
+        //방향키 입력한대로 움직이기 ← → (임시 ↓)
+        case "ArrowLeft":
+            movingElem.forEach((e) => {
+                e.classList.remove("moving");
+            });
+            movingElem.forEach((e) => {
+                e.previousSibling.classList.add("moving");
+            });
+            break;
 
-    for (let i = 0; i < rowCount; i++) {
-        let rowElem = document.createElement("div");
-        rowElem.className = "row";
+        case "ArrowRight":
+            movingElem.forEach((e) => {
+                e.classList.remove("moving");
+            });
+            movingElem.forEach((e) => {
+                e.nextSibling.classList.add("moving");
+            });
+            break;
 
-        for (let j = 0; j < columnCount; j++) {
-            let columnElem = document.createElement("div");
-            columnElem.className = "column";
-            rowElem.prepend(columnElem);
-        }
+        case "ArrowDown":
 
-        document.querySelector(".board").prepend(rowElem);
+        case "ArrowUp":
+        //도형 모양 바꾸기 ↑
     }
+});
+
+//테트리스 판 작성
+function init() {
+    for (let i = 0; i < rowCount; i++) {
+        addNewRow();
+    }
+}
+
+//새 Row 추가
+function addNewRow() {
+    let rowElem = document.createElement("div");
+    rowElem.className = "row";
+
+    for (let j = 0; j < columnCount; j++) {
+        let columnElem = document.createElement("div");
+        columnElem.className = "column";
+        rowElem.prepend(columnElem);
+    }
+
+    board.prepend(rowElem);
 }
